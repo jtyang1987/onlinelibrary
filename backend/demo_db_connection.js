dbb06ef32f9f510a7bbebf92b1c4376e77472a63
nkgxgongxi <nkgxgongxi@gmail.com>
2023-02-11 23:01:07 +1100

    FIX: Remove Credentials to DB

--- backend/demo_db_connection.js
+++ backend/demo_db_connection.js
@@ -1,13 +1,7 @@
-//https://www.w3schools.com/nodejs/nodejs_mysql.asp
+//https://www.w3schools.com/nodejs/nodejs_mysql.asp 
 
 var mysql = require('mysql');
 var config = require('./config')
-
-// var con = mysql.createConnection({
-//   host: "node-mysql-db.cj6ugv9emffe.ap-northeast-1.rds.amazonaws.com",
-//   user: "libraryAdmin",
-//   password: "YangLib123!"
-// });
 
 var con = mysql.createConnection({
   host: config.mysql.host,
